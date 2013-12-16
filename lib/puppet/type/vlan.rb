075e3c0c97cc7a50ba364c04fa646b740d44cac5
sharninder khera <sharninder_khera@dell.com>
2013-12-16 09:55:31 +0000

    Removing unused properties from type.

--- lib/puppet/type/vlan.rb
+++ lib/puppet/type/vlan.rb
@@ -20,15 +20,4 @@ Puppet::Type.newtype(:vlan) do
       desc "VLAN name."
     end
 
-    newproperty(:interface) do
-      desc "Interfaces to be added to the vlan"
-    end
-
-    newproperty(:portchannel) do
-      desc "portchannels"
-    end
-    
-    newparam(:device_url) do
-      desc "The URL of the router or switch maintaining this VLAN."
-    end
 end
