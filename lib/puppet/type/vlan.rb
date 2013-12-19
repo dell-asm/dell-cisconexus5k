7af372acd420f0290dcbd5099c3acf8208e42fce
sharninder khera <sharninder_khera@dell.com>
2013-12-19 13:17:45 +0000

    Adding a few validations to the vlan type

--- lib/puppet/type/vlan.rb
+++ lib/puppet/type/vlan.rb
@@ -15,14 +15,21 @@ Puppet::Type.newtype(:vlan) do
         if value !~ /^\d+/
           raise ArgumentError, "The VLAN Id should be positive integer."
         end
-        if value.to_i == 0
-          raise ArgumentError, "The VLAN Id 0 is invalid."
+        if value.to_i <= 1 || value.to_i >= 4094
+          raise ArgumentError, "The VLAN Id should be in the range 2-4094."
         end
+        #if value.to_i == 0
+        #  raise ArgumentError, "The VLAN Id 0 is invalid."
+        #end
       end
     end
 
     newproperty(:vlanname) do
       desc "VLAN name."
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "VLAN name property cannot be empty."
+        end
+      end
     end
-
 end
