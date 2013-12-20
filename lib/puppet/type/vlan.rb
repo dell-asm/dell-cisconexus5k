77d7c41477d1aae78d2f9736fbf78731bbb1660a
BISWAJIT RAUT <BISWAJIT_RAUT@dell.com>
2013-12-20 09:06:11 +0000

    Added resource types for interface and portchannel

--- lib/puppet/type/vlan.rb
+++ lib/puppet/type/vlan.rb
@@ -15,8 +15,8 @@ Puppet::Type.newtype(:vlan) do
         if value !~ /^\d+/
           raise ArgumentError, "The VLAN Id should be positive integer."
         end
-        if value.to_i <= 1 || value.to_i >= 4093
-          raise ArgumentError, "The VLAN Id should be in the range 1-4093."
+        if value.to_i <= 1 || value.to_i >= 4094
+          raise ArgumentError, "The VLAN Id should be in the range 2-4094."
         end
         #if value.to_i == 0
         #  raise ArgumentError, "The VLAN Id 0 is invalid."
@@ -32,4 +32,175 @@ Puppet::Type.newtype(:vlan) do
         end
       end
     end
+
+    newproperty(:istrunkforinterface) do
+      desc "Trunking is true or false for interface."
+      newvalues(:true, :false)
+      defaultto(:true)
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newproperty(:interface) do
+      desc "The interface name."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The Interface is invalid."
+        end
+      end
+    end
+
+    newproperty(:interfaceoperation) do
+      desc "The interface operation."
+      newvalues(:add, :remove)
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newproperty(:interfaceencapsulationtype) do
+      desc "Encapsulationtype for interface."
+      defaultto "dot1q"
+      munge do |value|
+        value.to_s
+      end
+    end
+
+    newproperty(:isnative) do
+      desc "The interface is associated with native vlan or not"
+      newvalues(:true, :false)
+      defaultto(:true)
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newproperty(:nativevlanid) do
+      desc "The native vlan id."
+      defaultto "1"
+      munge do |value|
+        value.to_i
+      end
+
+      validate do |value|
+        if value !~ /^\d+/
+          raise ArgumentError, "The native VLAN Id should be positive integer."
+        end
+        if value.to_i == 0
+          raise ArgumentError, "The native VLAN Id 0 is invalid."
+        end
+      end
+    end
+
+    newproperty(:removeallassociatedvlans) do
+      desc "Remove all associated vlans or not."
+      newvalues(:true, :false)
+      defaultto(:true)
+      munge do |value|
+        value.to_s
+      end
+    end
+
+    newproperty(:deletenativevlaninformation) do
+      desc "Delete native vlan information or not."
+      newvalues(:true, :false)
+      defaultto(:true)
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newproperty(:unconfiguretrunkmode) do
+      desc "Un configure trunk mode or not."
+      newvalues(:true, :false)
+      defaultto(:true)
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newproperty(:shutdownswitchinterface) do
+      desc "Shutdown switch interface or or not."
+      newvalues(:true, :false)
+      defaultto(:true)
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newproperty(:portchannel) do
+      desc "Port channel number."
+
+      validate do |value|
+        if value !~ /^\d+/
+          raise ArgumentError, "The Port channelId should be positive integer."
+        end
+        if value.to_i == 0
+          raise ArgumentError, "The Port channelId 0 is invalid."
+        end
+      end
+    end
+
+    newproperty(:portchanneloperation) do
+      desc "The port channel operation."
+      newvalues(:add, :remove)
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newproperty(:istrunkforportchannel) do
+      desc "Trunking is true or false for portchannel."
+      newvalues(:true, :false)
+      defaultto(:true)
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newproperty(:portchannelencapsulationtype) do
+      desc "Encapsulationtype for portchannel."
+      defaultto "dot1q"
+      munge do |value|
+        value.to_s
+      end
+    end
+
 end
