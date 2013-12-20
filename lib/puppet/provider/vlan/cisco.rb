3c06a8917362804b7c773138eb78971f9bfada50
BISWAJIT RAUT <BISWAJIT_RAUT@dell.com>
2013-12-20 09:12:30 +0000

    Code modified for interface and portchannel

--- lib/puppet/provider/vlan/cisco.rb
+++ lib/puppet/provider/vlan/cisco.rb
@@ -21,7 +21,25 @@ Puppet::Type.type(:vlan).provide :cisconexus5k, :parent => Puppet::Provider::Cis
   # Clear out the cached values.
   def flush
     device.command do |dev|
+      interface = ""
+      portchannel = ""
+      interface = resource[:interface]
+      portchannel = resource[:portchannel]
+      ensureabsent = resource[:ensure]
+      interfaceoperation = resource[:interfaceoperation]
+      if ( interface != nil && ensureabsent == :absent)
+        dev.update_interface(resource[:name], former_properties, properties,resource[:interface],resource[:nativevlanid],resource[:istrunkforinterface],resource[:interfaceencapsulationtype],resource[:isnative],resource[:deletenativevlaninformation],resource[:unconfiguretrunkmode],resource[:shutdownswitchinterface],resource[:interfaceoperation])
+      end
+      if ( portchannel != nil && ensureabsent == :absent)
+        dev.update_portchannel(resource[:name],former_properties, properties,resource[:portchannel],resource[:istrunkforportchannel],resource[:portchanneloperation])
+      end
       dev.update_vlan(resource[:name], former_properties, properties)
+      if ( interface != nil && ensureabsent == :present)
+        dev.update_interface(resource[:name], former_properties, properties,resource[:interface],resource[:nativevlanid],resource[:istrunkforinterface],resource[:interfaceencapsulationtype],resource[:isnative],resource[:deletenativevlaninformation],resource[:unconfiguretrunkmode],resource[:shutdownswitchinterface],resource[:interfaceoperation])
+      end
+      if ( portchannel != nil && ensureabsent == :present)
+        dev.update_portchannel(resource[:name],former_properties, properties,resource[:portchannel],resource[:istrunkforportchannel],resource[:portchanneloperation])
+      end
     end
     super
   end
